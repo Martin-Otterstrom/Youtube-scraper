6f1ffa59b5ec0335ac4367defaea0b4e7de4e709
Martin <78563493+Martin-Otterstrom@users.noreply.github.com>
2022-03-18 23:06:29 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+/* Use this inside "Apps script" in Google Docs, make sure to ad the youtube Data API in "services */
+
+function YoutubeScraper() {
+  var spreadSheet = SpreadsheetApp.getActiveSpreadsheet()
+  var activeSheet = spreadSheet.getActiveSheet()
+
+  var search = YouTube.Search.list(`snippet, id`, {q: "vlog 1", maxResults: 10}) /* Read the api for more complex info*/
+
+  var results = search.items.map((item) => [item.id.videoId, item.snippet.title, item.snippet.channelTitle, item.snippet.publishedAt])
+
+  var ids = results.map((id) => id[0]).join(",")
+
+  var stats = YouTube.Videos.list("statistics", {id: ids})
+
+  var videoStats = stats.items.map((item) => [item.statistics.viewCount, item.statistics.likeCount])
+
+  activeSheet.getRange(2, 1, results.length, results[0].length).setValues(results)
+  activeSheet.getRange(2, 5, videoStats.length, videoStats[0].length).setValues(videoStats)
+}
+
+/*
+If you find getRange(row, column, optNumRows, optNumColumns) arguments confusing:
+row --- int --- top row of the range
+column --- int--- leftmost column of the range
+optNumRows --- int --- number of rows in the range.
+optNumColumns --- int --- number of columns in the range
+*/
